4073f3ad8bff35ffe56c7984de16661b5e24d512
LoYiLun <37390225+LoYiLun@users.noreply.github.com>
2019-04-23 00:32:49 +0800

    20190423_2
    1.更新了創建資料夾的代碼，舊的是錯的。

--- 有序批量創建資料夾/Main.py
+++ 有序批量創建資料夾/Main.py
@@ -1,7 +1,7 @@
 import os
 print("please input dataPath")
-dirPath = "C:/Users/user/Desktop/新增資料夾/ADT104101"
-#os.chdir(dirPath)
+dirPath = str(input())
+os.chdir(dirPath)
 
 print("please input data name")
 dirName = str(input())
@@ -14,8 +14,8 @@ indexE = int(input())
 
 for i in range(indexB,indexE+1):
     if(i < 10):
-        fp = open(dirName + '0' +str(i) + '.docx',mode='w')
-        fp.close()
+        os.mkdir(dirName + '0' +str(i))
+        
     else:   
-        fp = open(dirName +str(i) + '.docx')
-        fp.close()
+        os.mkdir(dirName + str(i))
+        
